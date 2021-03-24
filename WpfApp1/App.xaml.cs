a89a20d170556af2f913cfe92476ca1d76e09530
Андрей Пахнин <a.pahnin@mxmgt.com>
2021-03-24 20:33:13 +0300

    Add project files.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        int i = 1;
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            int ii = 2;
+        }
+    }
+}
